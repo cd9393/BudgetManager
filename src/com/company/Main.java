0090d1ba9a0967b5eda60d7a51dc9d3d3c92940b
Craig Dunlop <craigdunlop@craigs-air.home>
2021-10-13 21:27:40 +0100

    refactored and broke logic into methods

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -6,20 +6,36 @@ import java.util.ArrayList;
 public class Main {
     public static void main(String[] args) {
         // write your code here
-        Scanner scanner = new Scanner(System.in);
-        ArrayList<String> userInput = new ArrayList<String>();
-        while(scanner.hasNext()) {
-            userInput.add(scanner.nextLine());
-        }
+        ArrayList<String> userInput = buildUserInput();
+        outPutUserInput(userInput);
+        Double total = returnTotal(userInput);
+        System.out.println("total: $" +  total);
+
+    }
+
+    static Double returnTotal(ArrayList<String> userInput) {
         Double total = 0.00;
 
         for (int i = 0; i < userInput.size(); i++) {
             total += getPrice(userInput.get(i));
+        }
+        return total;
+    }
+
+    static void outPutUserInput(ArrayList<String> userInput) {
+        for (int i = 0; i < userInput.size(); i++) {
             System.out.println(userInput.get(i));
         }
+    }
 
-        System.out.println("total: $" +  total);
-
+    static ArrayList<String> buildUserInput() {
+        try (Scanner scanner = new Scanner(System.in)) {
+            ArrayList<String> userInput = new ArrayList<String>();
+            while(scanner.hasNext()) {
+                userInput.add(scanner.nextLine());
+            }
+            return userInput;
+        }
     }
 
     static double getPrice(String input) {
